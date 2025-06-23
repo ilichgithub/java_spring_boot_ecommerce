cd7e7bc264684c5a7005b5601d37549ec20c8a8d
ilichgithub <ilichrondon@gmail.com>
2025-06-23 09:28:50 -0400

    Mappers de Category y Product

---
+++ src/main/java/com/ilich/sb/e_commerce/mapper/IProductMapper.java
@@ -0,0 +1,28 @@
+package com.ilich.sb.e_commerce.mapper;
+
+import java.util.List;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
+
+import com.ilich.sb.e_commerce.dto.ProductDTO;
+import com.ilich.sb.e_commerce.model.Product;
+
+@Mapper(componentModel = "spring")
+public interface IProductMapper {
+
+    IProductMapper INSTANCE = Mappers.getMapper(IProductMapper.class);
+
+    @Mapping(source = "category", target = "categoryDTO")
+    ProductDTO toDto(Product product);
+
+    @Mapping(source = "categoryDTO", target = "category")
+    Product toEntity(ProductDTO productDTO);
+    
+    @Mapping(source = "category", target = "categoryDTO")
+    List<ProductDTO> toDtoList(List<Product> categories);
+
+    @Mapping(source = "categoryDTO", target = "category")
+    List<Product> toEntityList(List<ProductDTO> categoriesDTO);
+}
